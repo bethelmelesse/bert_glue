f40ac39e34fd5b370dd413c36b5abd8699d0d40a
Bethel <28558632+bettymelesse@users.noreply.github.com>
2023-01-02 00:41:50 +0900

    added run script for running BERT with GLUE datasets

---
+++ run.sh
@@ -0,0 +1,11 @@
+python run_glue.py \
+  --model_name_or_path prajjwal1/bert-mini \
+  --task_name qnli \
+  --do_train \
+  --do_eval \
+  --max_seq_length 128 \
+  --per_device_train_batch_size 64 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --save_steps 50000 \
+  --output_dir tmp_qnli
